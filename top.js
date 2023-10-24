8059eb81ef9e54e90d5519b815c5d6f64b8d71c1
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-10-24 13:13:03 +0000

    js scroll

--- top.js
+++ top.js
@@ -1,16 +1,11 @@
-// ページ内リンクのみ取得します。
-$('a[href^=#]').click(function(){
-    //デフォルトのイベントをキャンセル
-    event.preventDefault();
-
-    // 移動先となる要素を取得します。
-    var target = $(this.hash);
-    if (!target.length) return;
-
-    // 移動先の位置を取得します
-    var targetY = target.offset().top;
-
-    // animateで移動します
-    $('body').animate({scrollTop: targetY}, 10, 'swing');
-
+$(function(){
+  $('a[href^="#"]').click(function(){
+    var adjust = 0;
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top + adjust;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
 });
