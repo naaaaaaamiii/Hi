79eef78df3128f3af89f802ecfed930cdcc4f295
naaamiiiiiii <1867031@g.iuhw.ac.jp>
2023-10-24 12:20:37 +0000

    js add

--- top.js
+++ top.js
@@ -1,1 +1,16 @@
-alert('Hello World');
+// ページ内リンクのみ取得します。
+$('a[href^=#]').click(function(){
+    //デフォルトのイベントをキャンセル
+    event.preventDefault();
+
+    // 移動先となる要素を取得します。
+    var target = $(this.hash);
+    if (!target.length) return;
+
+    // 移動先の位置を取得します
+    var targetY = target.offset().top;
+
+    // animateで移動します
+    $('body').animate({scrollTop: targetY}, 10, 'swing');
+
+});
